cb94cd40a60b8e134fd79c06a1fb68a50c51f2d2
ZekLouis <louis.gaume@etu.unilim.fr>
2017-06-10 21:54:45 +0200

    First commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<html>
+    <head>
+        <title>mgxpics</title>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="css/index.css">
+    </head>
+
+    <body>
+        <div class="header">
+            <h1>mgxpics</h1>
+        </div>
+    </body>
+</html>
