5e5874abdedcafd62ed5aaf5826025b364049f1c
ZekLouis <louis.gaume@etu.unilim.fr>
2017-06-11 00:09:31 +0200

    first update

--- index.php
+++ index.php
@@ -3,11 +3,75 @@
         <title>mgxpics</title>
         <meta charset="utf-8">
         <link rel="stylesheet" href="css/index.css">
+        <script src="js/jquery-3.2.1.min.js"></script>
     </head>
 
     <body>
-        <div class="header">
-            <h1>mgxpics</h1>
+        <div class="header-wrap">
+            <div class="header">
+                <div class="image"></div>
+                <div class="content">
+                    <h1>mgxpics</h1>
+                    <div class="divider"></div>
+                    <h2>photograph lovers</h2>
+                </div>
+            </div>
         </div>
+
+        <div class="global">
+            <div class="img-wrapper">
+                <div class="image">
+                    <img src="images/img.jpg" alt="">
+                </div>
+            </div>
+
+            <div class="img-wrapper">
+                <div class="image">
+                    <img src="images/header2.jpg" alt="">
+                </div>
+            </div>
+<div class="img-wrapper">
+                <div class="image">
+                    <img src="images/header2.jpg" alt="">
+                </div>
+            </div>
+<div class="img-wrapper">
+                <div class="image">
+                    <img src="images/header2.jpg" alt="">
+                </div>
+            </div>
+<div class="img-wrapper">
+                <div class="image">
+                    <img src="images/header2.jpg" alt="">
+                </div>
+            </div>
+<div class="img-wrapper">
+                <div class="image">
+                    <img src="images/header2.jpg" alt="">
+                </div>
+            </div>
+<div class="img-wrapper">
+                <div class="image">
+                    <img src="images/header2.jpg" alt="">
+                </div>
+            </div>
+<div class="img-wrapper">
+                <div class="image">
+                    <img src="images/header2.jpg" alt="">
+                </div>
+            </div>
+<div class="img-wrapper">
+                <div class="image">
+                    <img src="images/header2.jpg" alt="">
+                </div>
+            </div>
+<div class="img-wrapper">
+                <div class="image">
+                    <img src="images/header2.jpg" alt="">
+                </div>
+            </div>
+
+        </div>
+
     </body>
 </html>
