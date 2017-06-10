bcf68a00db7c9525c6d371d1e9606eb9ccbeccaf
ZekLouis <louis.gaume@etu.unilim.fr>
2017-06-11 00:52:54 +0200

    added navbar footer

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
         <title>mgxpics</title>
         <meta charset="utf-8">
         <link rel="stylesheet" href="css/index.css">
+        <link rel="stylesheet" href="css/font-awesome.min.css">
         <script src="js/jquery-3.2.1.min.js"></script>
     </head>
 
@@ -16,6 +17,14 @@
                     <h2>photograph lovers</h2>
                 </div>
             </div>
+        </div>
+
+        <div id="navbar">
+            <span class="left">mgxpics</span>
+            <span class="right">
+                <a href="https://instagram.com/mgxpics"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+                <a href="https://facebook.com/mgxpics"><i class="fa fa-facebook-square" aria-hidden="true"></i></a>
+            </span>
         </div>
 
         <div class="global">
@@ -73,5 +82,49 @@
 
         </div>
 
+        <footer>
+            <div class="content">
+                <div class="col-footer">
+                    <ul>
+                        <li>aze</li>
+                        <li>aze</li>
+                        <li>aze</li>
+                        <li>aze</li>
+                    </ul>
+                </div>
+
+                <div class="col-footer">
+                    <ul>
+                        <li>aze</li>
+                        <li>aze</li>
+                        <li>aze</li>
+                        <li>aze</li>
+                    </ul>
+                </div>
+
+                <div class="col-footer">
+                    <ul>
+                        <li>aze</li>
+                        <li>aze</li>
+                        <li>aze</li>
+                        <li>aze</li>
+                    </ul>
+                </div>
+            </div>
+        </footer>
+
+        <script>
+            $(document).ready(function() {
+                var height = window.innerHeight;
+                $(window).scroll(function () {
+                    if ($(window).scrollTop() > height) {
+                        $('#navbar').addClass('fixed');
+                    }
+                    if ($(window).scrollTop() < height+1) {
+                        $('#navbar').removeClass('fixed');
+                    }
+                });
+            });
+        </script>
     </body>
 </html>
