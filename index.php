9e914beef8e5cd571b27ee893b04120798a8e691
ZekLouis <louis.gaume@etu.unilim.fr>
2017-06-13 18:33:02 +0200

    Updated design

--- index.php
+++ index.php
@@ -4,7 +4,13 @@
         <meta charset="utf-8">
         <link rel="stylesheet" href="css/index.css">
         <link rel="stylesheet" href="css/font-awesome.min.css">
+        <link rel="stylesheet" href="css/animate.css">
         <script src="js/jquery-3.2.1.min.js"></script>
+        <script src="js/jquery.parallax-1.1.3.js"></script>
+        <script src="js/jquery.localscroll-1.2.7-min.js"></script>
+        <script src="js/jquery.scrollTo-1.4.2-min.js"></script>
+        <script src="js/viewportchecker.js"></script>
+
     </head>
 
     <body>
@@ -28,93 +34,176 @@
         </div>
 
         <div class="global">
-            <div class="img-wrapper">
+            <div class="img-wrapper" data-av-animation="fadeIn">
                 <div class="image">
+                    <span>
+                        <p>beetle</p>
+                    </span>
                     <img src="images/img.jpg" alt="">
                 </div>
             </div>
 
-            <div class="img-wrapper">
+            <div class="img-wrapper" data-av-animation="fadeIn">
                 <div class="image">
+                    <span>
+                        <p>rails</p>
+                    </span>
                     <img src="images/header2.jpg" alt="">
                 </div>
             </div>
-<div class="img-wrapper">
+            <div class="img-wrapper" data-av-animation="fadeIn">
                 <div class="image">
+                    <span>
+                        <p>rails</p>
+                    </span>
                     <img src="images/header2.jpg" alt="">
                 </div>
             </div>
-<div class="img-wrapper">
+            <div class="img-wrapper" data-av-animation="fadeIn">
                 <div class="image">
+                    <span>
+                        <p>rails</p>
+                    </span>
                     <img src="images/header2.jpg" alt="">
                 </div>
             </div>
-<div class="img-wrapper">
+            <div class="img-wrapper" data-av-animation="fadeIn">
                 <div class="image">
+                    <span>
+                        <p>rails</p>
+                    </span>
                     <img src="images/header2.jpg" alt="">
                 </div>
             </div>
-<div class="img-wrapper">
+            <div class="img-wrapper" data-av-animation="fadeIn">
                 <div class="image">
+                    <span>
+                        <p>rails</p>
+                    </span>
                     <img src="images/header2.jpg" alt="">
                 </div>
             </div>
-<div class="img-wrapper">
+            <div class="img-wrapper" data-av-animation="fadeIn">
                 <div class="image">
+                    <span>
+                        <p>rails</p>
+                    </span>
                     <img src="images/header2.jpg" alt="">
                 </div>
             </div>
-<div class="img-wrapper">
+            <div class="img-wrapper" data-av-animation="fadeIn">
                 <div class="image">
+                    <span>
+                        <p>rails</p>
+                    </span>
                     <img src="images/header2.jpg" alt="">
                 </div>
             </div>
-<div class="img-wrapper">
+            <div class="img-wrapper" data-av-animation="fadeIn">
                 <div class="image">
+                    <span>
+                        <p>rails</p>
+                    </span>
                     <img src="images/header2.jpg" alt="">
                 </div>
             </div>
-<div class="img-wrapper">
+            <div class="img-wrapper" data-av-animation="fadeIn">
                 <div class="image">
+                    <span>
+                        <p>rails</p>
+                    </span>
+                    <img src="images/header2.jpg" alt="">
+                </div>
+            </div>
+            <div class="img-wrapper" data-av-animation="fadeIn">
+                <div class="image">
+                    <span>
+                        <p>rails</p>
+                    </span>
+                    <img src="images/header2.jpg" alt="">
+                </div>
+            </div>
+            <div class="img-wrapper" data-av-animation="fadeIn">
+                <div class="image">
+                    <span>
+                        <p>rails</p>
+                    </span>
+                    <img src="images/header2.jpg" alt="">
+                </div>
+            </div>
+            <div class="img-wrapper" data-av-animation="fadeIn">
+                <div class="image">
+                    <span>
+                        <p>rails</p>
+                    </span>
                     <img src="images/header2.jpg" alt="">
                 </div>
             </div>
 
+
+
+
         </div>
+
+        <section class="we">
+            <h3>Who are we ?</h3>
+
+            <div class="desc-wrapper">
+                <div class="mathilde">
+                    <img src="images/mathilde.jpg" alt="">
+                    <p class="name">Mathilde</p>
+                    <p>Mathilde 🌞 G💘</p>
+                </div>
+            </div>
+
+            <div class="desc-wrapper">
+                <div class="gabriel">
+                    <img src="images/gabriel.jpg" alt="">
+                    <p class="name">Gabriel</p>
+                    <p>Gab💥🌊 16 yo | chtx📍| jean gi📚 | basketball🏀 | M.</p>
+                </div>
+            </div>
+
+        </section>
 
         <footer>
             <div class="content">
                 <div class="col-footer">
+                    <h3>Instagram</h3>
                     <ul>
-                        <li>aze</li>
-                        <li>aze</li>
-                        <li>aze</li>
-                        <li>aze</li>
+                        <li>@mgxpics</li>
+                        <li>@gaby_2k</li>
+                        <li>@mathilde_pclt</li>
                     </ul>
                 </div>
 
                 <div class="col-footer">
+                    <h3>Facebook</h3>
                     <ul>
-                        <li>aze</li>
-                        <li>aze</li>
-                        <li>aze</li>
-                        <li>aze</li>
+                        <li>MGXpics</li>
                     </ul>
                 </div>
 
                 <div class="col-footer">
+                    <h3>Copyrights</h3>
                     <ul>
-                        <li>aze</li>
-                        <li>aze</li>
-                        <li>aze</li>
-                        <li>aze</li>
+                        <li>Made in France</li>
+                        <li>2017 © All rights reserved</li>
                     </ul>
                 </div>
             </div>
         </footer>
 
         <script>
+            jQuery('.img-wrapper').addClass("hidden").viewportChecker({
+                classToAdd: 'visible animated fadeIn', // Class to add to the elements when they are visible
+                offset: 100
+            });
+
             $(document).ready(function() {
+                $(".we").parallax("50%", 0.1);
+                $(".header .image").parallax("50%", 0.1);
+                
                 var height = window.innerHeight;
                 $(window).scroll(function () {
                     if ($(window).scrollTop() > height) {
